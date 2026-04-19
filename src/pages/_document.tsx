c2ac52a9ac944dcd86c6804a90551c817e8ffeda
softgenai <admin@softgen.ai>
2026-04-19 13:25:45 +0000

    style(ui): update site branding assets

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,9 +4,9 @@ export default function Document() {
   return (
     <Html lang="de-CH">
       <Head>
-        {/* Favicon - The little red car */}
-        <link rel="icon" href="/buyauto-logo.png" />
-        <link rel="apple-touch-icon" href="/buyauto-logo.png" />
+        {/* Favicon - BuyAuto logo */}
+        <link rel="icon" href="/ChatGPT_Image_Mar_24_2026_02_28_34_PM.jpg" />
+        <link rel="apple-touch-icon" href="/ChatGPT_Image_Mar_24_2026_02_28_34_PM.jpg" />
         
         {/* Preconnect to Supabase storage for faster image loading */}
         <link rel="preconnect" href="https://psdtkknwxzxnxnbqmdzl.supabase.co" />
