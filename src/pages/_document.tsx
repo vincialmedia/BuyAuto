7c5c873d21a766e5cd3d4b995ce9d98308f98f7a
vincent-5618 <vincent@vincialmedia.com>
2026-04-20 14:42:48 +0000

    style(ui): update favicon reference

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,9 +4,9 @@ export default function Document() {
   return (
     <Html lang="de-CH">
       <Head>
-        {/* Favicon - BuyAuto logo */}
-        <link rel="icon" href="/buyauto-logo.jpg" />
-        <link rel="apple-touch-icon" href="/buyauto-logo.jpg" />
+        {/* Favicon - BuyAuto BA icon */}
+        <link rel="icon" href="/favicon-ba.png" />
+        <link rel="apple-touch-icon" href="/favicon-ba.png" />
         
         {/* Preconnect to Supabase storage for faster image loading */}
         <link rel="preconnect" href="https://psdtkknwxzxnxnbqmdzl.supabase.co" />
